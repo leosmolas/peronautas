c4f9fd4da2947df2b93dade3f6c50d413a066ae7
Diego <diegomarcov@gmail.com>
2011-04-13 16:56:27 -0300

    Agregados los archivos para correr el servidor al repo

---
+++ massim/scripts/startMarsMonitor.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if [ -z $1 ]; then
+	host=localhost
+else
+	host=$1
+fi
+
+if [ -z $2 ];then
+	port=1099
+else
+	port=$2
+fi
+
+java -Xss20000k -cp ../target/agentcontest-2011-1.0.jar massim.competition2011.monitor.GraphMonitor -rmihost $host -rmiport $port -savexmls 
