9214c772f2597a026186334d565fd6cafd6cd742
Manuel Torres <jmtorresluc@gmail.com>
2011-06-28 19:22:10 -0300

    Modificado la parte de delp. Gotti maraca.

--- pyeismassim/pl/delp/arg.pl
+++ pyeismassim/pl/delp/arg.pl
@@ -13,6 +13,7 @@ is_a_built_in(less(X,Y)).
 is_a_built_in(greaterEq(X,Y)).
 is_a_built_in(lessEq(X,Y)).
 is_a_built_in(equal(X,Y)).
+is_a_built_in(notEqual(X,Y)).
 
 
 mult(X,Y,Z):- Z is X * Y.
@@ -23,5 +24,5 @@ greater(X,Y):- X > Y.
 less(X,Y):- X < Y.
 greaterEq(X,Y):- X >= Y.
 lessEq(X,Y):- X =< Y.
-lessEq(X,Y):- X =:= Y.
-
+equal(X,Y):- X =:= Y.
+notEqual(X,Y):- X \= Y.
