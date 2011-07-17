d4906fbe2eef64e6f4fd16a6b578daa46632b4a6
Emiliano Montenegro <emm.montenegro@gmail.com>
2011-07-17 12:02:08 -0300

    acomodados los roles de repairer y sentinel. comentado los predicados de explorer.

--- pyeismassim/pl/repairer.pl
+++ pyeismassim/pl/repairer.pl
@@ -1,26 +1,128 @@
-step(Action) :-
-    handle_messages,
-    handle_percepts,
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                               Repairer                                %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Actions (priority order):
+%                   -repair
+%                   -survey
+%                   -goto
+%                   -parry  (no implemented)
+%                   -buy    (no implemented)
+%                   -rechage
+%                   -skip   (no implemented)
+
+%-----------------------------------------------------------------------%
+
+exec(Action) :- 
+    write(1),nl,
     action(Action).
 
-action(recharge) :-
-    % Plan: recharge.
-    % get energy level.
-    % if at full charge, stop recharging
-    % if energy < maxenergy / 3, recharge
-    true.
+%-----------------------------------------------------------------------%
 
-action(repair(Agent)) :-
-    % Repairing agent
-    % select needy agent in my actual position
-    Agent = something.
+reachableNode(Node, [[_, unknown] | T]) :-
+    reachableNode(Node, T),
+    !.
+reachableNode(Node, [[Node, Cost] | _T]) :-
+    Cost \= unknown,
+    energy(X),
+    X >= Cost,
+    !.
+reachableNode(Node, [_ | T]) :-
+    reachableNode(Node, T).
 
-action(goto(Vertex)) :-
-    % Random walking
-    % select neighbouring vertex
-    Vertex = something.
+%------------------------------  Repair  --------------------------------%
 
-action(skip).
+action([repair, Ally]):-
+    write(2),nl,
+    %obtengo mi energia
+    energy(X),
+    %chequeo si puedo realizar la accion de ataque { cost(attack)=2 }
+    write(2.1),write(' energy: '),write(X),nl,
+    X>1,
+    %obtengo mi nombre
+    myName(Name),
+    %obtengo mi posicion
+    write(2.2),nl,
+    k(position(Name, Position)),
+    %obtengo cual es mi equipo
+    write(2.3),nl,
+    agentTeam(Name, Team),
+    %obtengo el nombre de un agente que se encuentra en mi posicion
+    write(2.4),nl,
+    k(position(Ally, Position)),
+    Ally \= Name,
+    %veo que sea de mi equipo
+    write(2.5),nl,
+    agentTeam(Ally, Team),
+    %lo curo
+    !.
+    
+%------------------------------  Survey  --------------------------------%
+
+action([survey, Position]) :-
+    write(3),nl,
+    energy(X),
+    write(3.1),nl,
+    X > 0,
+    write(3.2),nl,
+    myName(Name),
+    write(3.3),nl,
+    k(position(Name, Position)),
+    write(3.4),nl,
+    hasAtLeastOneUnsurveyedEdge(Position), 
+    write(3.5),nl,
+    !.
+
+%-------------------------------  Goto  ---------------------------------%
+
+%-- First Node Goto --%
+
+action([goto, X]) :-
+    write(5),nl,
+    myName(Name),
+    write(5.1),nl,
+    k(position(Name, Position)),
+    write(5.2),nl,
+    energy(E),
+    write(5.3),nl,
+    k(edge(Position, X, Cost)),
+    Cost \= unknown,
+    write(5.4),nl,
+    E >= Cost, 
+    write(5.5),nl,
+    !.
+
+%-------------------------------  Recharge  ------------------------------%
+    
+action([recharge]) :-
+    write(6),nl.
+
+
+%-------------------------------  Old Code  ------------------------------%
+
+% step(Action) :-
+%    handle_messages,
+%    handle_percepts,
+%    action(Action).
+
+% action(recharge) :-
+%    % Plan: recharge.
+%    % get energy level.
+%    % if at full charge, stop recharging
+%    % if energy < maxenergy / 3, recharge
+%    true.
+
+% action(repair(Agent)) :-
+%    % Repairing agent
+%    % select needy agent in my actual position
+%    Agent = something.
+
+% action(goto(Vertex)) :-
+%    % Random walking
+%    % select neighbouring vertex
+%    Vertex = something.
+
+% action(skip).
 
 
 %  public class SimpleRepairerAgent extends Agent {
--- pyeismassim/pl/sentinel.pl
+++ pyeismassim/pl/sentinel.pl
@@ -1,23 +1,117 @@
-step(Action) :-
-    handle_messages,
-    handle_percepts,
-    action(Action).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                               Sentinel                                %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-action(recharge) :-
-    % Plan: recharge.
-    % get energy level.
-    % if at full charge, stop recharging
-    % if energy < maxenergy / 3, recharge
-    true.
+%Actions (priority order):
+%                   -survey
+%                   -goto
+%                   -parry  (no implemented)
+%                   -buy    (no implemented)
+%                   -rechage
+%                   -skip   (no implemented)
 
-action(buy(battery)) :-
-    % Plan: buy battery.
-    % get money
-    % if money < 10, we do not have enough money
-    % otherwise but battery
-    true.
+%-----------------------------------------------------------------------%
 
-action(survey) :-
+reachableNode(Node, [[_, unknown] | T]) :-
+    reachableNode(Node, T),
+    !.
+reachableNode(Node, [[Node, Cost] | _T]) :-
+    Cost \= unknown,
+    energy(X),
+    X >= Cost,
+    !.
+reachableNode(Node, [_ | T]) :-
+    reachableNode(Node, T).
+
+%------------------------------  Repair  --------------------------------%
+
+action([repair, Ally]):-
+    write(2),nl,
+    %obtengo mi energia
+    energy(X),
+    %chequeo si puedo realizar la accion de ataque { cost(attack)=2 }
+    write(2.1),write(' energy: '),write(X),nl,
+    X>1,
+    %obtengo mi nombre
+    myName(Name),
+    %obtengo mi posicion
+    write(2.2),nl,
+    k(position(Name, Position)),
+    %obtengo cual es mi equipo
+    write(2.3),nl,
+    agentTeam(Name, Team),
+    %obtengo el nombre de un agente que se encuentra en mi posicion
+    write(2.4),nl,
+    k(position(Ally, Position)),
+    Ally \= Name,
+    %veo que sea de mi equipo
+    write(2.5),nl,
+    agentTeam(Ally, Team),
+    %lo curo
+    !.
+    
+%------------------------------  Survey  --------------------------------%
+
+action([survey, Position]) :-
+    write(3),nl,
+    energy(X),
+    write(3.1),nl,
+    X > 0,
+    write(3.2),nl,
+    myName(Name),
+    write(3.3),nl,
+    k(position(Name, Position)),
+    write(3.4),nl,
+    hasAtLeastOneUnsurveyedEdge(Position), 
+    write(3.5),nl,
+    !.
+
+%-------------------------------  Goto  ---------------------------------%
+
+%-- First Node Goto --%
+
+action([goto, X]) :-
+    write(5),nl,
+    myName(Name),
+    write(5.1),nl,
+    k(position(Name, Position)),
+    write(5.2),nl,
+    energy(E),
+    write(5.3),nl,
+    k(edge(Position, X, Cost)),
+    Cost \= unknown,
+    write(5.4),nl,
+    E >= Cost, 
+    write(5.5),nl,
+    !.
+
+%-------------------------------  Recharge  ------------------------------%
+    
+action([recharge]) :-
+    write(6),nl.
+
+%-------------------------------  Old Code  ------------------------------%
+
+% step(Action) :-
+%    handle_messages,
+%    handle_percepts,
+%    action(Action).
+
+% action(recharge) :-
+%    % Plan: recharge.
+%    % get energy level.
+%    % if at full charge, stop recharging
+%    % if energy < maxenergy / 3, recharge
+%    true.
+
+% action(buy(battery)) :-
+%    % Plan: buy battery.
+%    % get money
+%    % if money < 10, we do not have enough money
+%    % otherwise but battery
+%    true.
+
+% action(survey) :-
     %   println("I know " + getAllBeliefs("visibleEdge").size() + " visible edges");
     %   println("I know " + getAllBeliefs("surveyedEdge").size() + " surveyed edges");
     %
@@ -69,10 +163,10 @@ action(survey) :-
     %   return null;
         
 
-action(goto(Vertex)) :-
+% action(goto(Vertex)) :-
     % Random walking
     % select neighbouring vertex
-    Vertex = something.
+%    Vertex = something.
 
-action(skip).
+% action(skip).
 
