4c351c33d310f4bbd9b957b04faf577692447f8e
Leo Molas <leos.molas@gmail.com>
2011-07-19 22:31:44 -0300

    Trayendo el kmap modificado

--- pyeismassim/pl/kmap.pl
+++ pyeismassim/pl/kmap.pl
@@ -5,60 +5,34 @@
 % RandomSeed = 1
 % Number of nodes = 15 (si, tiene 14 x ser simetrico).
 
-myName(evita).
-% k(position(vasco, vertex0)).
+knode(vertex0,  unknown, 10). knode(vertex9,  unknown, 10).
+knode(vertex1,  unknown,  5). knode(vertex11, unknown,  5).
+knode(vertex3,  unknown,  5). knode(vertex7,  unknown,  5).
+knode(vertex4,  unknown,  4). knode(vertex2,  unknown,  4).
+knode(vertex8,  unknown,  3). knode(vertex6,  unknown,  3).
+knode(vertex10, unknown,  4). knode(vertex5,  unknown,  4).
+knode(vertex12, unknown,  2). knode(vertex13, unknown,  2).
 
-my_team(peronismo).
+kedge(vertex5,  vertex8,  4). kedge(vertex6,  vertex10, 4).
+kedge(vertex8,  vertex7,  1). kedge(vertex3,  vertex6,  1).
+kedge(vertex2,  vertex5,  9). kedge(vertex4,  vertex10, 9).
+kedge(vertex2,  vertex8,  9). kedge(vertex6,  vertex4,  9).
+kedge(vertex5,  vertex7,  9). kedge(vertex3,  vertex10, 9).
+kedge(vertex2,  vertex7,  6). kedge(vertex3,  vertex4,  6).
 
-k(position(peron, vertex8)).
-k(position(evita, vertex13)).
-k(position(menem, vertex7)).
-k(position(cafiero, vertex0)).
+kedge(vertex5,  vertex1,  9). kedge(vertex10, vertex11, 9).
+kedge(vertex5,  vertex0,  5). kedge(vertex9,  vertex10, 5).
+kedge(vertex0,  vertex7,  2). kedge(vertex3,  vertex9,  2).
+kedge(vertex1,  vertex0,  7). kedge(vertex9,  vertex11, 7).
+kedge(vertex1,  vertex13, 6). kedge(vertex12, vertex11, 6).
+kedge(vertex13, vertex0,  6). kedge(vertex9,  vertex12, 6).
 
-k(position(iorio, vertex9)).
+kedge(vertex8,  vertex6,  8).
+kedge(vertex7,  vertex3,  1).
+kedge(vertex0,  vertex9,  4).
+kedge(vertex13, vertex12, 2).
 
-k(node(vertex0 , peronismo, 10)). k(node(vertex9 , none, 10)).
-k(node(vertex1 , peronismo,  5)). k(node(vertex11, none,  5)).
-k(node(vertex3 , none,  5)). k(node(vertex7 , peronismo,  5)).
-k(node(vertex4 , none,  4)). k(node(vertex2 , peronismo,  4)).
-k(node(vertex8 , peronismo,  3)). k(node(vertex6 , none,  3)).
-k(node(vertex10, none,  4)). k(node(vertex5 , peronismo,  4)).
-k(node(vertex12, none,  2)). k(node(vertex13, peronismo,  2)).
 
-k(edge(vertex5, vertex8,  4)). k(edge(vertex6, vertex10, 4)).
-k(edge(vertex8, vertex7,  1)). k(edge(vertex3, vertex6,  1)).
-k(edge(vertex2, vertex5,  9)).  k(edge(vertex4, vertex10, 9)).
-k(edge(vertex2, vertex8,  9)). k(edge(vertex6, vertex4,  9)).
-k(edge(vertex5, vertex7,  9)). k(edge(vertex3, vertex10, 9)).
-k(edge(vertex2, vertex7,  6)). k(edge(vertex3, vertex4,  6)).
 
-k(edge(vertex5, vertex1,  9)). k(edge(vertex10, vertex11, 9)).
-k(edge(vertex5, vertex0,  5)). k(edge(vertex9, vertex10, 5)).
-k(edge(vertex0, vertex7,  2)). k(edge(vertex3, vertex9, 2)).
-k(edge(vertex1, vertex0,  7)). k(edge(vertex9, vertex11, 7)).
-k(edge(vertex1, vertex13, 6)). k(edge(vertex12, vertex11, 6)).
-k(edge(vertex13, vertex0, 6)). k(edge(vertex9, vertex12, 6)).
 
-k(edge(vertex6, vertex8, 8)).
-k(edge(vertex3, vertex7, 1)).
-k(edge(vertex9, vertex0, 4)).
-k(edge(vertex12, vertex13, 2)).
 
-k(edge(vertex8, vertex5, 4)). k(edge(vertex10, vertex6, 4)).
-k(edge(vertex7, vertex8, 1)). k(edge(vertex6, vertex3, 1)).
-k(edge(vertex5, vertex2, 9)).  k(edge(vertex10, vertex4, 9)).
-k(edge(vertex8, vertex2, 9)). k(edge(vertex4, vertex6, 9)).
-k(edge(vertex7, vertex5, 9)). k(edge(vertex10, vertex3, 9)).
-k(edge(vertex7, vertex2, 6)). k(edge(vertex4, vertex3, 6)).
-
-k(edge(vertex1, vertex5, 9)). k(edge(vertex11, vertex10, 9)).
-k(edge(vertex0, vertex5, 5)). k(edge(vertex10, vertex9, 5)).
-k(edge(vertex7, vertex0, 2)). k(edge(vertex9, vertex3, 2)).
-k(edge(vertex0, vertex1, 7)). k(edge(vertex11, vertex9, 7)).
-k(edge(vertex13, vertex1, 6)). k(edge(vertex11, vertex12, 6)).
-k(edge(vertex0, vertex13, 6)). k(edge(vertex12, vertex9, 6)).
-
-k(edge(vertex8, vertex6,  8)).
-k(edge(vertex7, vertex3,  1)).
-k(edge(vertex0, vertex9,  4)).
-k(edge(vertex13, vertex12, 2)).
