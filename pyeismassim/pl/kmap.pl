e039957839183cee96dbeec9aab16bffff3214aa
Leo Molas <leos.molas@gmail.com>
2011-07-28 15:47:03 -0300

    correcion del bfs en el otro branch

--- pyeismassim/pl/kmap.pl
+++ pyeismassim/pl/kmap.pl
@@ -104,6 +104,41 @@ k(edge(vertex7,  vertex3,  1)).
 k(edge(vertex0,  vertex9,  4)).
 k(edge(vertex13, vertex12, 2)).
 
+% Arcos invertidos
+
+k(edge(vertex8, vertex5, 4)).
+k(edge(vertex7, vertex8, 1)).
+k(edge(vertex5, vertex2, 9)).
+k(edge(vertex8, vertex2, 9)).
+k(edge(vertex7, vertex5, 9)).
+k(edge(vertex7, vertex2, 6)).
+
+k(edge(vertex10, vertex6, 4)).
+k(edge(vertex6, vertex3, 1)).
+k(edge(vertex10, vertex4, 9)).
+k(edge(vertex4, vertex6, 9)).
+k(edge(vertex10, vertex3, 9)).
+k(edge(vertex4, vertex3, 6)).
+
+k(edge(vertex1, vertex5, 9)).
+k(edge(vertex0, vertex5, 5)).
+k(edge(vertex7, vertex0, 2)).
+k(edge(vertex0, vertex1, 7)).
+k(edge(vertex13, vertex1, 6)).
+k(edge(vertex0, vertex13, 6)).
+
+k(edge(vertex11, vertex10, 9)).
+k(edge(vertex10, vertex9, 5)).
+k(edge(vertex9, vertex3, 2)).
+k(edge(vertex11, vertex9, 7)).
+k(edge(vertex11, vertex12, 6)).
+k(edge(vertex12, vertex9, 6)).
+
+k(edge(vertex6, vertex8, 8)).
+k(edge(vertex3, vertex7, 1)).
+k(edge(vertex9, vertex0, 4)).
+k(edge(vertex12, vertex13, 2)).
+
 
 %h(edge(vertex5,  vertex8,  4)). 
 %h(edge(vertex8,  vertex7,  1)). 
@@ -145,7 +180,7 @@ k(edge(vertex13, vertex12, 2)).
 
 currentStep(0).
 
-position(0, peron, vertex3).
+position(0, peron, vertex0).
 position(0, evita, vertex12).
 position(0, machinea, vertex10).
 position(0, cavallo, vertex6).
@@ -170,3 +205,4 @@ team(peronismo).
 team(cacoteam).
 
 myTeam(peronismo).
+myName(peron).
